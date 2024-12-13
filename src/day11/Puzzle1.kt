8e9fed6aee9bfff1e0607963bcd1f2f8267f18d9
Thijs Oostdam <thijs@oostd.am>
2024-12-13 18:50:51 +0200

    day 11_2

--- src/day11/Puzzle1.kt
+++ src/day11/Puzzle1.kt
@@ -7,56 +7,61 @@ fun main() {
 
     val lines = File(fileName).readLines()
 
-    var stones = lines[0].split(" ").map { nr -> nr.toLong() }
+    val stones = lines[0].split(" ").map { nr -> nr.toLong() }
 
-    var growth = 0.0
-    var size = stones.size
-    for (i in 0 until 75){
-        stones = blink(stones)
-        growth = stones.size.toDouble() / size.toDouble()
-        size = stones.size
-        println("${i+1}: ${stones.size}: $growth")
+    val cache = BlinkCache()
+    var sum = 0L;
+    for (stone in stones){
+        sum += cache.blink75(stone, 0)
     }
 
-    println(stones.size)
+    println(sum)
+}
 
-    // TODO
-    // keep track of each nr and how many stones they multiply into for each nr of blinks
-    //.then use this information to optimize
+// Using some sort of memoization with recursion. Not the cleanest code ':)
+// But very fast
+class BlinkCache {
+    val cache = mutableMapOf<Pair<Stone, Int>, Long>()
+    fun blink75(stone: Stone, depth: Int): Long {
+        val key = Pair(stone, depth)
+        val cachedCount = cache[key]
+        if (cachedCount == null) {
+            if (depth == 75) {
+                return 1
+            }
+            if (stone == 0L) {
+                val count = blink75(1, depth + 1)
+                cache[key] = count
+                return count
+            }
+            val splitStones = split(stone)
+            if (splitStones != null) {
+                val count = blink75(splitStones[0], depth + 1) + blink75(splitStones[1], depth + 1)
+                cache[key] = count
+                return count
+            }
+            val count = blink75(stone * 2024, depth + 1)
+            cache[key] = count
+            return count
+        }else{
+            return cachedCount
+        }
+    }
 }
 
 typealias Stone = Long
 typealias Stones = List<Stone>
 
-fun blink(stones: Stones): Stones{
-    val newStones = mutableListOf<Stone>()
-
-    for (stone in stones){
-        if(stone == 0L){
-            newStones.add(1)
-            continue
-        }
-        val splitStones = split(stone)
-        if(splitStones != null){
-            newStones.addAll(splitStones)
-            continue
-        }
-        newStones.add(stone * 2024)
-    }
-    return newStones
-}
-
-fun split(stone: Stone): Stones?{
+fun split(stone: Stone): Stones? {
     val asString = stone.toString()
     val length = asString.length
     val even = (length % 2) == 0
-    return if(even) {
-//        println("Splitting even: $stone, $length")
+    return if (even) {
         val mid = length / 2
         val firstHalf = asString.substring(0, mid).toLong()
         val secondHalf = asString.substring(mid).toLong()
         listOf(firstHalf, secondHalf)
-    }else{
+    } else {
         null
     }
 }
